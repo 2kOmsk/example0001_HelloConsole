398acd36898a590d7d3794ecee2eb24f1d35de4f
2kOmsk <119439142+2kOmsk@users.noreply.github.com>
2023-04-18 13:10:08 +0600

    Update itogi_1.cs

---
+++ itogi_1.cs
@@ -0,0 +1,23 @@
+lass Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите количество строк массива: ");
+        int n = int.Parse(Console.ReadLine());
+        string[] arr = new string[n];
+        string[] newArr = new string[n];
+        int count = 0;
+
+        Console.WriteLine("Введите элементы массива: ");
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = Console.ReadLine();
+            if (arr[i].Length <= 3)
+            {
+                newArr[count] = arr[i];
+                count++;
+            }
+        }
+    }
+}
+
